278c8d8a231b1d1f35fb593d9ac8e6b9e0709b40
khairul-1 <tapu.eee@gmail.com>
2023-11-25 13:43:45 +0600

    user interface schema & model

---
+++ src/models/User.ts
@@ -0,0 +1,61 @@
+import mongoose, { Document, Schema } from 'mongoose';
+
+interface FullName {
+  firstName: string;
+  lastName: string;
+}
+
+interface Address {
+  street: string;
+  city: string;
+  country: string;
+}
+
+interface Order {
+  productName: string;
+  price: number;
+  quantity: number;
+}
+
+interface User extends Document {
+  userId: number;
+  username: string;
+  password: string;
+  fullName: FullName;
+  age: number;
+  email: string;
+  isActive: boolean;
+  hobbies: string[];
+  address: Address;
+  orders: Order[];
+}
+
+const userSchema = new Schema<User>({
+  userId: { type: Number, required: true, unique: true },
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  fullName: {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+  },
+  age: { type: Number, required: true },
+  email: { type: String, required: true },
+  isActive: { type: Boolean, required: true },
+  hobbies: [{ type: String }],
+  address: {
+    street: { type: String, required: true },
+    city: { type: String, required: true },
+    country: { type: String, required: true },
+  },
+  orders: [
+    {
+      productName: { type: String, required: true },
+      price: { type: Number, required: true },
+      quantity: { type: Number, required: true },
+    },
+  ],
+});
+
+const UserModel = mongoose.model<User>('User', userSchema);
+
+export default UserModel;
